2f843d2c6ec9012faba060fa9f1239e76e943660
Thomas Nattestad <thomas.nattestad@gmail.com>
2014-06-01 21:14:47 -0700

    more config setup

--- server.js
+++ server.js
@@ -1,27 +1,15 @@
 var express = require('express');
 var app = express();
 var sys = require('sys');
-// var path = require('path');
 
 app.use("/styles", express.static(__dirname + "/styles"));
 app.use("/scripts", express.static(__dirname + "/scripts"));
+app.use("/libs", express.static(__dirname + "/libs"));
 
 var port = process.env.PORT || 3000;
 app.listen(port);
 
 app.get('/', function (req, res){
 	sys.log("showing / ");
-	res.render(__dirname + '/index.html', {});
+	res.sendfile(__dirname + '/index.html', {});
 });
-	
-app.get('/room/:roomName', function (req, res){
-
-});
-
-app.get('/newRoom/:roomName', function (req, res){
-
-});
-
-function addUrl(url, room){
-
-}
